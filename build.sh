28adc2f4ac9f95d0fe998c6db99f7978b845e5b5
Ruda <a.ruda@FACC39WD9KL6R.tld>
2026-01-17 14:15:26 -0500

    Auto-migrate and load fixture data on deploy

--- build.sh
+++ build.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 set -o errexit
 
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
-
-python manage.py collectstatic --noinput
+pip install -r requirements.txt
 python manage.py migrate
+python manage.py loaddata data/people_fixture.json
+python manage.py collectstatic --noinput
 
-# Load fixture data (safe to re-run)
-python manage.py loaddata data/people_fixture.json || true
 
 
